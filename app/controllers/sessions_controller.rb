9326a9bb8735859d6e7a49b97bad2d341829a1f7
SteveZhangF <zhangke3012@gmail.com>
2015-09-02 15:13:15 -0500

    finish login/out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,21 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in user
+      # 判断用户是否勾选remember me 复选框
+      params[:session][:remember_me] == '1'? remember(user) : forget(user)
+      
+      remember user
+      redirect_to user
       # 登入用户，然后重定向到用户的资料页面
     else
       flash[:danger] = 'Invalid email/password combination' # 不完全正确
       render 'new'    
     end
+  end
+  
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 end
